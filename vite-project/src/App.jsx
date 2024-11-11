0ee7913c9c7483273badd09dc5251f71f83e88a1
unknown <you@example.com>
2024-11-11 23:36:37 +0530

    MERNcompleted

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,51 @@
+import React,{useEffect,useState,useRef} from "react";
+
+import axios from "axios";
+export default function App(){
+  const [records,setRecords]=useState([]);
+  const[status,setStatus]=useState({});
+
+  const e_id =useRef();
+  const e_name=useRef();
+  const e_sal =useRef();
+
+  const postData=()=>{
+    axios.post("http://localhost:8080/insert",{"e_id":e_id.current.value,
+      "e_name":e_name.current.value,
+      "e_sal":e_sal.current.value,}).then((posRes)=>{
+        setStatus(posRes.data);
+      },(errRes)=>{
+        console.log(errRes);
+      });
+  };
+  useEffect(()=>{
+    axios.get("http://localhost:8080/fetch").then((posRes)=>{
+      setRecords(posRes.data);
+  },(errRes)=>{
+    console.log(errRes);
+  });
+  },[]);
+  return(
+    <div>
+    <table>
+      <tr>
+        <th>SNO</th>
+        <th>E_ID</th>
+        <th>E_Name</th>
+        <th>E_SAL</th>
+      </tr>
+      <tr>
+        <td>{index+1}</td>
+        <td>{obj.e_id}</td>
+        <td>{obj.e_name}</td>
+        <td>{obj.e_sal}</td>
+      </tr>
+    </table>
+    <input type="number" ref={e_id}/>
+    <input type="text" ref={e_name}/>
+    <input type="number" ref={e_sal}/>
+    <button onClick={postData}>save</button>
+    </div>
+    
+  )
+}
